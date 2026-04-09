0b75d0dd1545d0f32a2037f80c90f23914171237
Pedro Henrique Gomes Mello <devpedromello@gmail.com>
2026-04-09 00:44:42 -0300

    feat: add cors configuration and helmet

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ZodValidationPipe } from 'nestjs-zod';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,17 @@ async function bootstrap() {
   app.useGlobalPipes(new ZodValidationPipe());
   app.use(cookieParser());
 
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+  });
+
+  app.use(helmet({
+    contentSecurityPolicy: false,
+    crossOriginEmbedderPolicy: false
+  }))
+
+  await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
